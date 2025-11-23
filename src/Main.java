ebb8f6ac6078e20ab1e5813e6e24269b2bb3d8cb
zvakilov29 <zvakilov2018@gmail.com>
2025-11-23 13:23:06 +0400

    asdasd

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,8 @@ void main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
+
+
+        System.out.println("asdasdasd");
     }
 }
